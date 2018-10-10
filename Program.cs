844598c0dfd443e39d3f178dccb1e9ac8cd17285
Joseph <Jpatrick148@Gmail.com>
2018-10-10 15:31:26 -0400

    Lab 4 1st

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string check;
+
+            do
+            {
+
+                Console.WriteLine("Please enter an integer.");
+                int num = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Squared");
+                for (int power = 1; power <= num; power++)
+                    Console.WriteLine("{0}^{1} = {2:N0} ",
+                        num, power, (long) Math.Pow(num, power));
+
+                Console.WriteLine("Cubed");
+                for (int power = 1; power <= num; power++)
+                    Console.WriteLine("{0}^{1} = {2:N0}",
+                        num, power, (long) Math.Pow(num, power) * power);
+
+                DataTable dt = new DataTable();
+                dt.Columns.Add("Squared", typeof(int));
+                dt.Columns.Add("Cubed", typeof(int));
+
+
+
+
+                ////Console.WriteLine("Squared using method");
+                //for (int power = 1; power <= Square(num); power++)
+                //    Console.WriteLine("{0} ^ {1} = {2:N0}", num, power, Square(num));
+
+                num = 0;
+                Console.WriteLine("Would you like to try again? {Y/N}");
+                check = Console.ReadLine().ToLower();
+
+            }
+            while (check == "y");
+
+        }
+
+        public static int Square(int x)
+        {
+            int result = x * x;
+            return result;
+        }
+
+        public static int Cube(int x)
+        {
+            int result = x * x * x;
+            return result;
+        }
+
+
+    }
+}
